b56e0ade87ccbc1a29cc7201ab11031487a1d16e
Jordan Leung <jordanjj8@gmail.com>
2019-05-03 11:07:02 -0700

    generated multiple random walks and added an error case

---
+++ Random Walk/rw_visual.py
@@ -0,0 +1,35 @@
+# Jordan Leung
+# 5/3/2019
+# code to plot all the points in the walk
+
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# keep making new walks as long as the while loop stays true
+while True:
+
+    # Make a random walk instance, and plot the points
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, c=rw.x_values, cmap=plt.cm.Blues, s=15)
+    plt.show()
+
+# ask user if another walk to be plotted is desired
+    keep_running = input("Make another walk? (Y/N): ")
+    if keep_running == 'N':
+        break
+    elif keep_running == 'Y':
+        continue
+    else:
+        # catches any ambiguous answers
+        while True:
+            keep_running = input("Please type either Y or N: ")
+            if keep_running == 'N' or keep_running == 'Y':
+                break
+
+# make a second random walk instance
+#rj = RandomWalk(50)
+#rj.fill_walk()
+# s = 15 specifies the dot size
+#plt.scatter(rj.x_values, rj.y_values, c=rj.x_values, cmap=plt.cm.Blues, s=15)
+#plt.show()
